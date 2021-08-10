3414adef3aca82a2488ddb496f26d8a4e0c982c8
ssolo <s.solovev9208@gmail.com>
2021-08-10 20:49:27 +0300

    add my settings

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Deploy an AWS Lambda Function in Python using the Serverless Framework and GitHub Actions (CI/CD)
-
-### Full Video Tutorial
-* Check out the YouTube tutorial [here](https://youtu.be/KorJPUKvHKc)!
+# AWS LAMBDA HELLO WORLD
--- lambda.py
+++ lambda.py
@@ -1,12 +1,27 @@
-import requests
+import json
 
-def handler(event, context):
-    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-    res = {
-        "event": event,
-        "output": response.json(),
-        "context": context
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    context: object, required
+        Lambda Context runtime methods and attributes
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "HELLO WORLD",
+            }
+        ),
     }
-    print(res)
-
-    return None
